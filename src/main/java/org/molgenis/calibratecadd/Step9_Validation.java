327371910c0e2a54512045c327061253d5175cc2
K. Joeri van der Velde <joeriv@gmail.com>
2015-12-14 16:32:05 +0100

    Added the new benchmarks to the main program

--- src/main/java/org/molgenis/calibratecadd/Step9_Validation.java
+++ src/main/java/org/molgenis/calibratecadd/Step9_Validation.java
@@ -2,11 +2,14 @@ package org.molgenis.calibratecadd;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
+import org.molgenis.calibratecadd.support.MutationTaster2Results;
 import org.molgenis.calibratecadd.support.PONP2Results;
+import org.molgenis.calibratecadd.support.ProveanAndSiftResults;
 import org.molgenis.calibratecadd.support.VariantClassificationException;
 import org.molgenis.calibratecadd.support.JudgedVariant;
 import org.molgenis.calibratecadd.support.JudgedVariant.ExpertClassification;
@@ -37,9 +40,10 @@ public class Step9_Validation
 	 */
 	public Step9_Validation(String ccggLoc, String mvlLoc, String mode) throws Exception
 	{
-		if(!mode.equals("full") && !mode.equals("naiveonly") && !mode.equals("ponp2"))
+		List<String> modes = Arrays.asList(new String[]{"ccgg", "naive", "ponp2", "mutationtaster2", "provean", "sift"});
+		if(!modes.contains(mode))
 		{
-			throw new Exception("mode needs to be 'full', 'naiveonly', or 'ponp2'");
+			throw new Exception("mode needs to be one of : " + modes.toString());
 		}
 		loadCCGG(ccggLoc);
 		scanMVL(mvlLoc, mode);
@@ -58,14 +62,24 @@ public class Step9_Validation
 		java.util.Iterator<Entity> vcfRepoIter = vcfRepo.iterator();
 		
 		/**
-		 * Here, we let PON-P2 handle the classification and see what happens
-		 * Obviously, before this works, you must score your variant list with PON-P2
+		 * Here, we let PON-P2 or MutationTaster2 handle the classification and see what happens
+		 * Obviously, before this works, you must score your variant list with PON-P2 or MutationTaster2
 		 * And then link this file below
 		 */
 		PONP2Results p2r = null;
+		MutationTaster2Results m2r = null;
+		ProveanAndSiftResults ps2r = null;
 		if (mode.equals("ponp2"))
 		{
 			p2r = new PONP2Results(new File("/Users/jvelde/github/maven/molgenis-data-cadd/data/PONP2_predictions.txt"));
+		}
+		if (mode.equals("mutationtaster2"))
+		{
+			m2r = new MutationTaster2Results(new File("/Users/jvelde/github/maven/molgenis-data-cadd/data/MutationTaster2_output_minimized.tsv"));
+		}
+		if (mode.equals("provean") || mode.equals("sift"))
+		{
+			ps2r = new ProveanAndSiftResults(new File("/Users/jvelde/github/maven/molgenis-data-cadd/data/provean_sift_predictions_minimized.tsv"));
 		}
 
 		while (vcfRepoIter.hasNext())
@@ -110,7 +124,7 @@ public class Step9_Validation
 					try
 					{
 						Judgment judgment;
-						if(mode.equals("naiveonly"))
+						if(mode.equals("naive"))
 						{
 							judgment = ccgg.naiveClassifyVariant(gene, MAF, impact, CADDscore);
 						}
@@ -118,8 +132,21 @@ public class Step9_Validation
 						{
 							judgment = p2r.classifyVariantUsingPONP2Results(chr, pos, ref, alt);
 						}
+						else if (mode.equals("mutationtaster2"))
+						{
+							judgment = m2r.classifyVariantUsingMutationTaster2Results(chr, pos, ref, alt);
+						}
+						else if (mode.equals("provean"))
+						{
+							judgment = ps2r.classifyVariantUsingProveanResults(chr, pos, ref, alt);
+						}
+						else if (mode.equals("sift"))
+						{
+							judgment = ps2r.classifyVariantUsingSiftResults(chr, pos, ref, alt);
+						}
 						else
 						{
+							//"ccgg" mode, the default
 							judgment = ccgg.classifyVariant(gene, MAF, impact, CADDscore);
 						}
 						multipleJudgments.add(judgment);
