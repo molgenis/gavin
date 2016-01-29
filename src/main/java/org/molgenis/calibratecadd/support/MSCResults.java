2d04f7360eea7ddf31a48566e56bedae8ef1e2c3
K. Joeri van der Velde <joeriv@gmail.com>
2016-01-29 15:23:03 +0100

    Added MSC to tool comparison

---
+++ src/main/java/org/molgenis/calibratecadd/support/MSCResults.java
@@ -0,0 +1,65 @@
+package org.molgenis.calibratecadd.support;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import org.molgenis.data.annotation.joeri282exomes.Judgment;
+import org.molgenis.data.annotation.joeri282exomes.Judgment.Classification;
+import org.molgenis.data.annotation.joeri282exomes.Judgment.Method;
+
+public class MSCResults
+{
+	
+
+
+	//genename to MSC threshold
+	HashMap<String, Double> mscFile;
+	
+	/**
+	 * 
+	 */
+	public MSCResults(File mscResults) throws FileNotFoundException
+	{
+		mscFile = new HashMap<String, Double>();
+		Scanner s = new Scanner(mscResults);
+		s.nextLine(); //skip "Gene	CADD_MSC"
+		while(s.hasNextLine())
+		{
+			String line = s.nextLine();
+			String[] linesplit = line.split("\t", -1);
+			if(!linesplit[1].equals("NA"))
+			{
+				mscFile.put(linesplit[0], Double.parseDouble(linesplit[1]));
+			}
+		}
+		s.close();
+	}
+	
+	public Judgment classifyVariantUsingMSCResults(String geneName, Double caddScore) throws Exception
+	{
+		if(caddScore == null)
+		{
+			System.out.println(geneName);
+			throw new VariantClassificationException("CADD score of variant null");
+		}
+		else if(mscFile.containsKey(geneName))
+		{
+			if(caddScore > mscFile.get(geneName))
+			{
+				return new Judgment(Classification.Pathogn, Method.calibrated, "CADD score "+caddScore+" above MSC threshold " + mscFile.get(geneName));
+			}
+			else
+			{
+				return new Judgment(Classification.Benign, Method.calibrated, "CADD score "+caddScore+" below MSC threshold " + mscFile.get(geneName));
+			}
+		}
+		else
+		{
+			throw new VariantClassificationException("No MSC gene threshold available!");
+		}
+	}
+	
+	
+}
