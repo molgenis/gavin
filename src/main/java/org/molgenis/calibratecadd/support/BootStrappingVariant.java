97447a2f4faad570e8079b37b0f4968194a5f320
K. Joeri van der Velde <joeriv@gmail.com>
2016-05-11 16:22:09 +0200

    Updated bootstrap variant for latest analysis

--- src/main/java/org/molgenis/calibratecadd/support/BootStrappingVariant.java
+++ src/main/java/org/molgenis/calibratecadd/support/BootStrappingVariant.java
@@ -5,26 +5,44 @@ public class BootStrappingVariant
 	public enum OutCome{
 		TP, TN, FP, FN, VOUS
 	}
-	
-	boolean inCalibGene;
-	OutCome outcome;
 
-	public BootStrappingVariant(OutCome outcome, boolean inCalibGene)
+	public enum ExpClsf{
+		B, P
+	}
+
+	public enum Label{
+		C1_C2, C4, C3
+	}
+	
+	private Label label;
+	private OutCome outcome;
+	private ExpClsf expClsf;
+
+	public BootStrappingVariant(OutCome outcome, Label label, ExpClsf expClsf)
 	{
 		super();
 		this.outcome = outcome;
-		this.inCalibGene = inCalibGene;
+		this.label = label;
+		this.expClsf = expClsf;
 	}
 
-	public boolean isInCalibGene()
-	{
-		return inCalibGene;
+	public Label getLabel() {
+		return label;
 	}
 
 	public OutCome getOutcome()
 	{
 		return outcome;
 	}
-	
-	
+
+	public ExpClsf getExpClsf() { return expClsf; }
+
+	@Override
+	public String toString() {
+		return "BootStrappingVariant{" +
+				"label=" + label +
+				", outcome=" + outcome +
+				", expClsf=" + expClsf +
+				'}';
+	}
 }
