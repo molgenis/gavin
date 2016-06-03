15d7ffba05c1b09315b207a23f9e407cdc51c85e
K. Joeri van der Velde <joeriv@gmail.com>
2016-06-03 10:20:27 +0200

    Updated default location of performance output file

--- src/main/java/org/molgenis/calibratecadd/Step9_AllValidations.java
+++ src/main/java/org/molgenis/calibratecadd/Step9_AllValidations.java
@@ -12,7 +12,7 @@ public class Step9_AllValidations
 {
 	public static void main(String[] args) throws Exception
 	{
-		String outFile = "/Users/joeri/step9_panels_out.R";
+		String outFile = "/Users/joeri/github/gavin/data/other/step9_panels_out.R";
 		String path = "/Users/joeri/github/gavin/data/predictions";
 	//	List<String> datasets = Arrays.asList(new String[]{"ClinVarNew", "MutationTaster2", "UMCG_Onco", "UMCG_Various", "VariBenchTest", "VariBenchTraining"});
 		List<String> datasets = Arrays.asList(new String[]{"Renal", "Pulmonary", "Ophthalmologic", "Oncologic", "Obstetric", "NotInCGD", "Neurologic", "Musculoskeletal", "Hematologic", "Genitourinary", "General", "Gastrointestinal", "Endocrine", "Dermatologic", "Dental", "Craniofacial", "Cardiovascular", "Biochemical", "Audiologic_Otolaryngologic", "Allergy_Immunology_Infectious"});
