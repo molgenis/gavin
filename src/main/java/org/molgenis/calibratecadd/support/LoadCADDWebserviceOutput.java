aab0d0460897c89e1ea282acc9012338f4db139e
K. Joeri van der Velde <joeriv@gmail.com>
2016-06-15 11:07:12 +0200

    Fixed some problems with ref/alt trimming

--- src/main/java/org/molgenis/calibratecadd/support/LoadCADDWebserviceOutput.java
+++ src/main/java/org/molgenis/calibratecadd/support/LoadCADDWebserviceOutput.java
@@ -43,19 +43,28 @@ public class LoadCADDWebserviceOutput
 	 * ATGTG ATG -> ATG A
 	 * ATGTG ATGTGTGTG -> A ATGTG
 	 * GATAT GAT -> GAT G
+	 *
+	 * Examples:
+	 * GATA GATAGATA -> G GATAG
+	 * TTCTT T -> TTCTT T (don't touch)
 	 */
 	public static String trimRefAlt(String ref, String alt, String sep)
 	{
-		
+		// GATA -> ATAG
 		char[] refRev = StringUtils.reverse(ref).toCharArray();
+		// GATAGATA -> ATAGATAG
 		char[] altRev = StringUtils.reverse(alt).toCharArray();
 		
 		int nrToDelete = 0;
-		for(int i = 0; i < refRev.length; i++)
+		//iterate over: A, T, A. Do not touch the last reference base (G).
+		for(int i = 0; i < refRev.length-1; i++)
 		{
 			char refBase = refRev[i];
 			char altBase = altRev[i];
-			if(refBase == altBase)
+
+			//altRev.length > i+1 prevents the last matching alt base from being/attempted deleted, e.g. TTCTT_T -> TTCT_
+			//this may happen because we iterate over reference, which may be longer in case of a deletion
+			if(refBase == altBase && altRev.length > i+1)
 			{
 				nrToDelete++;
 			}
@@ -66,8 +75,8 @@ public class LoadCADDWebserviceOutput
 		}
 		String newRef = ref.substring(0, ref.length()-nrToDelete);
 		String newAlt = alt.substring(0, alt.length()-nrToDelete);
-		
-//		System.out.println("turning '" + ref + " " + alt + "' into '" + newRef + " " + newAlt + "'");
+
+		//result: GATA GATAGATA -> G GATAG
 		return newRef + sep + newAlt;
 	}
 	
